673afcf10f80665eab1cff3aae381f42f8e63f66
jose-rgb <josericardosaraiva7@gmail.com>
2022-03-23 19:09:22 -0300

    criando primeira rota #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'articles#index'
+
+  #quando houver uma req nessa rota, redirecionar para controler articles action index
+  get '/articles', to: 'articles#index'
 end
