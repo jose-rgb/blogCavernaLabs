9c2c7ef2e50bd94efb18f84be6ac28aefda87667
jose-rgb <josericardosaraiva7@gmail.com>
2022-03-27 15:12:50 -0300

    exibindo um article  #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
-  #quando houver uma req nessa rota, redirecionar para controler articles action index
-  get '/articles', to: 'articles#index'
+  resources :articles
 end
