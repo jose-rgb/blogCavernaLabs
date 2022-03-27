7c251f65ca5be51c7c29f1d866011f97a4128c5e
jose-rgb <josericardosaraiva7@gmail.com>
2022-03-27 20:46:41 -0300

    criando um article  #6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,37 @@
 class ArticlesController < ApplicationController
   
+  #mostrar todos
   def index
     @articles = Article.all 
   end
 
+  #mostrar um
   def show
     @article = Article.find(params[:id])
   end
 
+  #criar
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      #redireciona para show
+      redirect_to @article
+    else
+      #renderizar a view new carregando o article
+      render :new
+    end
+
+  end
+
+  private
+
+  #params permitidos
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
