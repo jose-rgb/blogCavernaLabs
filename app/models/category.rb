5c89d80bcf8035c22714db56c6168561fdbd101a
jose-rgb <josericardosaraiva7@gmail.com>
2022-03-29 14:08:13 -0300

    gerando category  #13

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    #nome obrigatorio, tamanho min = 3
+    validates :name, presence: true, length: {minimum: 3}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_221134) do
+ActiveRecord::Schema.define(version: 2022_03_29_170123) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
