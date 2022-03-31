16583df3aaf57dea79ca7ea357182f90bca3c320
jose-rgb <josericardosaraiva7@gmail.com>
2022-03-31 14:31:19 -0300

    add mascara no email do user #22

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     def render_if(condition, template, record)
         render template, record if condition
     end
+
+    def sub_masked_email(email)
+        email.gsub(/(?<=.{4}).*@.*(?=\S{3})/, '***@*****')
+    end
 end
